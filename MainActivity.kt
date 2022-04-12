c280d559e011172d4eb82a2f8987a60ef8641e03
KDW999 <hide4321@naver.com>
2022-04-12 18:59:56 +0900

    Mentality test

--- MainActivity.kt
+++ MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.lovetest.fragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.example.lovetest.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+//val value
+//var variation
+
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = nav_host_fragment.findNavController()
     }
 }
